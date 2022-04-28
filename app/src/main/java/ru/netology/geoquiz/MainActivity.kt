5f0f9c06ebff143bc7440248d27eae4d38617c37
apexnt <apexnt@yandex.ru>
2022-04-28 13:43:27 +0500

    commit

---
+++ app/src/main/java/ru/netology/geoquiz/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.netology.geoquiz
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    @SuppressLint("ShowToast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
+        }
+    }
+}
